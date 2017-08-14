242a1738a77142aeb4a7d741deb20d87b480c2c3
Juhi Jariwala <juhi.jari@gmail.com>
2017-08-14 16:00:11 +0530

    Parameterized instance_type, spot_price and number_of_producers

--- create_producer_spot_instance.py
+++ create_producer_spot_instance.py
@@ -2,9 +2,13 @@ import boto3
 import base64
 
 brokerIP=input("Please enter brokerIP:")
+instanceType=input("Please enter instance type:")
+spotPrice=input("Please enter spot price:")
+numberOfProducers=input("PLease enter number of producers you want to run on this machine:")
 client = boto3.client('ec2')
-file = open('producerUserData.sh', 'r')
+file = open('producer_user_data.sh', 'r')
 userData=file.read().replace("$BROKER_IP",brokerIP)
+userData=userData.replace("$NUMBER_OF_PRODUCERS",numberOfProducers)
 
 encodedUserData=base64.b64encode(userData.encode())
 response = client.request_spot_instances(
@@ -15,7 +19,7 @@ response = client.request_spot_instances(
         ],
         'EbsOptimized': False,
         'ImageId': 'ami-c73f44a8',
-        'InstanceType': 'c4.xlarge',
+        'InstanceType': instanceType,
 
         'Placement': {
             'AvailabilityZone': 'ap-south-1b',
@@ -24,7 +28,5 @@ response = client.request_spot_instances(
         'SubnetId': 'subnet-d010849d',
         'UserData': str(encodedUserData.decode('utf-8'))
     },
-    SpotPrice='0.06'
+    SpotPrice=spotPrice
 )
-
-print(response)
--- producer_user_data.sh
+++ producer_user_data.sh
@@ -5,6 +5,6 @@ GENERATOR_PATH=/home/ec2-user/kafka_load_generator/json-data-generator-1.2.2-SNA
 sed -i -e "s/\"broker.server\": .*,/\"broker.server\": \"$BROKER_IP\",/g" $GENERATOR_PATH/conf/cpuUsageConfig_firstCluster.json
 
 CMD="java -jar -Xmx512m $GENERATOR_PATH/json-data-generator-1.2.2-SNAPSHOT.jar cpuUsageConfig_firstCluster.json"
-for run in {1..30}
+for run in {1..$NUMBER_OF_PRODUCERS}
   do $CMD > /dev/null  2&>1 &
 done
