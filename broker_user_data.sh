eeb32b056a2997201e88ed1251d8bf60b749b78c
Priyam Mitra <pmitra43@gmail.com>
2017-08-17 12:24:06 +0530

    Fixing broker user_data

--- broker_user_data.sh
+++ broker_user_data.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
-JMX_PATH="/root/broker/"
-KAFKA_PATH="/root/kafka_2.11-0.11.0.0/"
+set -ex
 MY_IP="$(ifconfig | grep -A 1 'eth0' | tail -1 | cut -d ':' -f 2 | cut -d ' ' -f 1)"
 
-if [[ -n `grep "ip-.*localhost" /etc/hosts` ]]; then
-  sed -i "s/^ip-.*localhost$/`hostname` $MY_IP localhost/" /etc/hosts
+if [[ -n `grep "10.*localhost" /etc/hosts` ]]; then
+  sed -i "s/^10.*localhost$/$MY_IP `hostname` localhost/" /etc/hosts
 else
-  echo "`hostname` $MY_IP localhost" >> /etc/hosts
+  echo "$MY_IP `hostname` localhost" >> /etc/hosts
+fi
+
+JMX_PATH="/root/broker"
+KAFKA_PATH="/root/kafka_2.11-0.11.0.0"
 
 sed -i "s/#*listeners=PLAINTEXT:\/\/.*:9092/listeners=PLAINTEXT:\/\/$MY_IP:9092/" $KAFKA_PATH/config/server.properties
 sed -i 's/zookeeper.connect=.*/zookeeper.connect=$ZOOKEEPER_IP:2181/' $KAFKA_PATH/config/server.properties
 
-JMXTRANS_OPTS="-Dport1=55555 -Durl1=localhost -DinfluxUrl=http://10.0.0.83:8086/ -DinfluxDb=kafka -DinfluxUser=admin -DinfluxPwd=admin" JAR_FILE=$JMX_PATH/jmxtrans-259-all.jar $JMX_PATH/jmxtrans.sh start
-JMX_PORT=55555 $KAFKA_PATH/bin/kafka-server-start.sh $KAFKA_PATH/config/server.properties > broker_log.log &
+JMX_PORT=55555 $KAFKA_PATH/bin/kafka-server-start.sh $KAFKA_PATH/config/server.properties > $KAFKA_PATH/broker_log.log &
+sleep 1m
+cd $JMX_PATH
+JMXTRANS_OPTS="-Dport1=55555 -Durl1=localhost -DinfluxUrl=http://10.0.0.83:8086/ -DinfluxDb=kafka -DinfluxUser=admin -DinfluxPwd=admin" JAR_FILE=jmxtrans-259-all.jar ./jmxtrans.sh start
