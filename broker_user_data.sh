0763628b9dd1ce8f2dffdc1438d9dd84deea8221
Priyam Mitra <pmitra43@gmail.com>
2017-08-17 16:34:54 +0530

    Adding SECONDS_BETWEEN_RUNS parameter to broker_user_data jmxtrans run command

--- broker_user_data.sh
+++ broker_user_data.sh
@@ -15,6 +15,5 @@ sed -i "s/#*listeners=PLAINTEXT:\/\/.*:9092/listeners=PLAINTEXT:\/\/$MY_IP:9092/
 sed -i 's/zookeeper.connect=.*/zookeeper.connect=$ZOOKEEPER_IP:2181/' $KAFKA_PATH/config/server.properties
 
 JMX_PORT=55555 $KAFKA_PATH/bin/kafka-server-start.sh $KAFKA_PATH/config/server.properties > $KAFKA_PATH/broker_log.log &
-sleep 1m
 cd $JMX_PATH
-JMXTRANS_OPTS="-Dport1=55555 -Durl1=localhost -DinfluxUrl=http://10.0.0.83:8086/ -DinfluxDb=kafka -DinfluxUser=admin -DinfluxPwd=admin" JAR_FILE=jmxtrans-259-all.jar ./jmxtrans.sh start
+JMXTRANS_OPTS="-Dport1=55555 -Durl1=localhost -DinfluxUrl=http://10.0.0.83:8086/ -DinfluxDb=kafka -DinfluxUser=admin -DinfluxPwd=admin" SECONDS_BETWEEN_RUNS=15 JAR_FILE=jmxtrans-259-all.jar ./jmxtrans.sh start
