d9d5f74487ef4eb73fb6434a258b16b6cab96865
Priyam Mitra <pmitra43@gmail.com>
2017-08-17 16:57:36 +0530

    Renaming and parameterizing broker and consumer boto scripts

--- create_broker_spot_instance.py
+++ create_broker_spot_instance.py
@@ -1,11 +1,13 @@
 import boto3
 import base64
 
-client=boto3.client('ec2')
-brokerIP=input("Please enter brokerIP:")
+client = boto3.client('ec2')
+zookeeperIP=input("Please enter zookeeperIP: ")
+instanceType=input("Enter instance type: ")
+spotPrice=input("Please enter spot price: ")
 
-file=open("consumer_user_data.sh","r")
-userData=file.read().replace("$BROKER_IP",brokerIP)
+file=open("broker_user_data.sh","r")
+userData=file.read().replace("$ZOOKEEPER_IP",zookeeperIP)
 
 encodedUserData=base64.b64encode(userData.encode())
 
@@ -13,20 +15,20 @@ response = client.request_spot_instances(
     InstanceCount=1,
     LaunchSpecification={
         'SecurityGroupIds': [
-            'sg-e6d7188e'
+            'sg-633ff10b'
         ],
         'KeyName': 'New_MS_POC',
         'EbsOptimized': False,
-        'ImageId': 'ami-09146e66',
-        'InstanceType': 'r4.xlarge',
+        'ImageId': 'ami-3f88f350',
+        'InstanceType': instanceType,
         'Placement': {
             'AvailabilityZone': 'ap-south-1b',
             'Tenancy': 'default',
         },
-        'SubnetId': 'subnet-d010849d',
+        'SubnetId': 'subnet-1e128653',
         'UserData': encodedUserData.decode('utf-8'),
     },
-    SpotPrice='0.06'
+    SpotPrice=spotPrice
 )
 
 print(response)
--- create_consumer_spot_instance.py
+++ create_consumer_spot_instance.py
@@ -1,11 +1,13 @@
 import boto3
 import base64
 
-client = boto3.client('ec2')
-zookeeperIP=input("Please enter zookeeperIP:")
+client=boto3.client('ec2')
+brokerIP=input("Please enter bootstrap-server IP: ")
+instanceType=input("Enter instance type: ")
+spotPrice=input("Please enter spot price: ")
 
-file=open("broker_user_data.sh","r")
-userData=file.read().replace("$ZOOKEEPER_IP",zookeeperIP)
+file=open("consumer_user_data.sh","r")
+userData=file.read().replace("$BROKER_IP",brokerIP)
 
 encodedUserData=base64.b64encode(userData.encode())
 
@@ -13,17 +15,17 @@ response = client.request_spot_instances(
     InstanceCount=1,
     LaunchSpecification={
         'SecurityGroupIds': [
-            'sg-633ff10b'
+            'sg-e6d7188e'
         ],
         'KeyName': 'New_MS_POC',
         'EbsOptimized': False,
-        'ImageId': 'ami-3f88f350',
+        'ImageId': 'ami-09146e66',
         'InstanceType': 'r4.xlarge',
         'Placement': {
             'AvailabilityZone': 'ap-south-1b',
             'Tenancy': 'default',
         },
-        'SubnetId': 'subnet-1e128653',
+        'SubnetId': 'subnet-d010849d',
         'UserData': encodedUserData.decode('utf-8'),
     },
     SpotPrice='0.06'
